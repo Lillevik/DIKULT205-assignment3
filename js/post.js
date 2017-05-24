63069dc10c1f95a9383b451b9edda79d5480e52a
Lillevik <mariusfalch@hotmail.com>
2017-05-24 10:34:27 +0200

    Readded post.js params

--- js/post.js
+++ js/post.js
@@ -3,6 +3,31 @@
  * all the comment appending to the parents.
  */
 
+
+var currentShown = null;
+const params = get_url_parameters();
+var working = false;
+
+function get_url_parameters() {
+        result = {};
+
+            var uri = document.location.href;
+
+            if (uri.indexOf("?") < 0)
+                return result;
+        query_string = uri.substring(uri.indexOf("?"));
+        if (query_string.indexOf("#") >= 0)
+                query_string = query_string.substring(0, query_string.indexOf("#"));
+
+
+                query_pattern = /(?:[?&])([^&=]+)(?:=)([^&]*)/g;
+        decode = function(str) {return decodeURIComponent(str.replace(/\+/g, " "));};
+        while(match = query_pattern.exec(query_string))
+                result[decode(match[1])] = decode(match[2]);
+        return result;
+}
+
+
 /**
  * This function displays a new textarea when the user
  * clicks on a reply button for a child comment.
